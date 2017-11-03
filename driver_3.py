c4339531b4be017ee21c53c5cebf36e21222a66d
LauritsDixen <33333137+LauritsDixen@users.noreply.github.com>
2017-11-03 19:30:00 +0100

    Update driver_3.py

--- driver_3.py
+++ driver_3.py
@@ -1,33 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  2 19:44:03 2017
-
-@author: lauritsdixen
-"""
-
 class Board(object):
-    # board class
+    # Board class
     
     def __init__(self,board):
-        # initiates a boardstate
+        # Initiates a Board
         self.board = board
        
     def getBoard(self):
-        # returns boardlist
+        # Returns copy of board(list) - not for changing the board
         return self.board[:]
     
     def __str__(self):
-        # prints the boardstate in a fancy way, for debugging
+        # Prints the boardstate in a fancy way, for debugging
         board = self.getBoard()
         return str(board[0:3]) + '\n' + str(board[3:6]) + '\n' + str(board[6:9])
        
     def getFreePosisition(self):
-        # returns integer that is the free place at the board
+        # Returns integer that is the free place at the board
         return self.getBoard().index(0)
     
     def getAvailMoves(self):
-        # return a list of strings with available moves
+        # Return a list of strings with available moves
         moves = []
         fP = self.getFreePosisition()
         
@@ -42,7 +34,7 @@ class Board(object):
         return moves
     
     def move(self,move):
-        # makes a move, take a string and outputs a list with the new boardstate
+        # Makes a move, take a move(string) and outputs a list with the new boardstate
         
         board = self.getBoard()
 
@@ -70,23 +62,102 @@ class Board(object):
             print("not a legal move") 
     
     def getChildren(self):
-        # return a list of boardstates(class)
-        # that can be generated from current state  
+        # Return a list of boardstates(class)
+        # That can be generated from current state  
         children = []
         
         for m in self.getAvailMoves():
-            children.append(Board(self.move(m)))
+            children.append(self.move(m))
         
         return children
+    
 
-     
-#if __name__ == "__main__":
-#    # execute only if run as a script
-#    main()      
-#
-#import sys
-#
-#print 'Number of arguments:', len(sys.argv), 'arguments.'
-#print 'Argument List:', str(sys.argv)   
+class Solver(object):
+    # Parent class for solvers
+    
+    def __init__(self,startState):
+        # Creates a new solver with start and goal
+        self.startState = startState
+        self.goalState = [0,1,2,3,4,5,6,7,8]
+        self.explored = []
+        self.frontier = [self.startState]
+        
+    def getStart(self):
+        # Returns starting state as Board(class)
+        return self.startState
+    
+    def getGoal(self):
+        # Returns the goal state as Board(class)
+        return self.goalState
+        
+    def getExplored(self):
+        # Returns list of boards(list) that has been explored
+        return self.explored
+    
+    def explore(self,state):
+        # Explore a state(Boards): puts it into the explored, check if its goal
+        # Return boolean for Succes or Failure
+        self.explored.append(state.getBoard())
+        
+        if state.getBoard() == self.getGoal():
+            return True
+        else:
+            return False
+        
+    def getFrontier(self):
+        # Returns the Boards that is ready to explored
+        # The way they are represented may differ for different solvers
+        # Start state is automaticly in there
+        return self.frontier
         
         
+class BFS(Solver):
+    # Breadth first search solver
+    def __init__(self,StartState):
+        # Inits a BFS solver using Solver init.
+        Solver.__init__(self,StartState)
+        
+    def chooseNextBoard(self):
+        # Removes last state in queue and returns it
+        return self.getFrontier().pop()
+    
+    def addToFrontier(self,board):
+        # Takes a state and puts it in queue, doesnt return anything
+        return self.getFrontier().insert(0,board)
+
+    
+class DFS(Solver):
+    # Depth first search solver
+    pass
+
+class AStar(Solver):
+    # A* solver
+    pass
+
+def runSolver(solverType, startState):
+    # The main function that control solver
+    # Takes a solver type (BFS, DFS, AStar)
+    # Returns some cool stuff ??
+    
+    #Creates a solver
+    solver = solverType(startState)
+    
+    # Main loop (checks if frontier is empty)
+    while len(solver.getFrontier()) != 0:
+            
+        # Gets new state from queue
+        state = Board(solver.chooseNextBoard())
+        
+        # Explore current state
+        if solver.explore(state):
+            print("Succes!")
+            print("Phew! That took ", str(time.time()-beginning_time))
+            return state
+        
+        # Puts possible children in the frontier
+        for c in state.getChildren():
+            if not (c in solver.getExplored() and c in solver.getFrontier()):
+                solver.addToFrontier(c)
+    
+    print("Failure!")
+    return solver.getExplored()
