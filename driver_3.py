955ac68773665f7fb15442eb201bdfba17d49d60
LauritsDixen <33333137+LauritsDixen@users.noreply.github.com>
2017-11-03 09:08:27 +0100

    Update driver_3.py

--- driver_3.py
+++ driver_3.py
@@ -15,14 +15,12 @@ class Board(object):
        
     def getBoard(self):
         # returns boardlist
-        return self.board
+        return self.board[:]
     
     def __str__(self):
         # prints the boardstate in a fancy way, for debugging
         board = self.getBoard()
-        out = str(board[0:3]) + '\n' + str(board[3:6]) + '\n' + str(board[6:9])
-        
-        return out
+        return str(board[0:3]) + '\n' + str(board[3:6]) + '\n' + str(board[6:9])
        
     def getFreePosisition(self):
         # returns integer that is the free place at the board
@@ -33,9 +31,9 @@ class Board(object):
         moves = []
         fP = self.getFreePosisition()
         
-        if fP - 3 <= 8:
+        if fP > 2:
             moves.append('Up')
-        if fP + 3 >= 0:
+        if fP < 6:
             moves.append('Down')
         if not fP % 3 == 0:
             moves.append('Left')
@@ -46,31 +44,30 @@ class Board(object):
     def move(self,move):
         # makes a move, take a string and outputs a list with the new boardstate
         
-        fP = self.getFreePosisition()
         board = self.getBoard()
 
         if move == 'Up':
-            partnerValue = board[fP - 3]
-            board[fP] = partnerValue
-            board[board.index(partnerValue)] = 0
+            a, b = board.index(0), board.index(0) - 3
+            board[b], board[a] = board[a], board[b]
+            return board
         
         if move == 'Down':
-            partnerValue = board[fP + 3]
-            board[fP] = partnerValue
-            board[board.index(partnerValue)] = 0
+            a, b = board.index(0), board.index(0) + 3
+            board[b], board[a] = board[a], board[b]
+            return board
             
         if move == 'Left':
-            partnerValue = board[fP - 1]
-            board[fP] = partnerValue
-            board[board.index(partnerValue)] = 0
+            a, b = board.index(0), board.index(0) - 1
+            board[b], board[a] = board[a], board[b]
+            return board
         
         if move == 'Right':
-            partnerValue = board[fP + 1]
-            board[fP] = partnerValue
-            board[board.index(partnerValue)] = 0
-            
-        return board
+            a, b = board.index(0), board.index(0) + 1
+            board[b], board[a] = board[a], board[b]
+            return board
         
+        else:
+            print("not a legal move") 
     
     def getChildren(self):
         # return a list of boardstates(class)
@@ -81,19 +78,15 @@ class Board(object):
             children.append(Board(self.move(m)))
         
         return children
-        
-b = Board([1,2,3,4,0,5,6,7,8])
-print ("current state: ")
-print(b)
-print(" ")
-b.move("Right")
-print(b)
-
 
      
 #if __name__ == "__main__":
 #    # execute only if run as a script
 #    main()      
-        
+#
+#import sys
+#
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)   
         
         
